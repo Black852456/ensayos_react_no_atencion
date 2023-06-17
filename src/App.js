cab3b84d0e20619a8ad656c6d61128de81040ef5
Brayan <becg852456@gmail.com>
2023-06-17 09:51:57 -0500

    Ejemplo de componentes con un dado

--- src/App.js
+++ src/App.js
@@ -1,60 +1,32 @@
  import logo from './logo.svg';
 import './App.css';
+import Dado from './Dado'
 import { useState } from 'react';
 
 
 function App() {
 
-    function eliminarUltimaFila(){
-      if(articulos.length>0){
-        const temp = Array.from(articulos);
-        temp.pop();
-        setArticulos(temp);
-      }
-    }
+    //let valor1 = Math.trunc(Math.random()*6)+1;
+    let valor1 = Math.trunc(Math.random()*6)+1;
+    let valor2 = Math.trunc(Math.random()*6)+1;
+    let valor3 = Math.trunc(Math.random()*6)+1;
 
-    const [articulos,setArticulos] = useState([
-      {
-        codigo:1,
-        descripcion:"Cebolla",
-        precio:12.45
-      },
-      {
-        codigo:2,
-        descripcion:"Zanahorias",
-        precio:4.15
-      },
-      {
-        codigo:3,
-        descripcion:"Pimenton",
-        precio:7.20
-      }
-    ]);
+  function CambiarValor(){
+    setValor(
+      [Math.trunc(Math.random()*6)+1,
+        Math.trunc(Math.random()*6)+1,
+        Math.trunc(Math.random()*6)+1]
+    );
+  }
+
+    let [valor,setValor] = useState([0,0,0])
 
     return (
     <div>
-      <table border="1">
-        <thead>
-          <tr>
-            <th>Codigo</th>
-            <th>Descripcion</th>
-            <th>Precio</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            articulos.map(articulo =>
-              <tr>
-                <th>{articulo.codigo}</th>
-                <th>{articulo.descripcion}</th>
-                <th>{articulo.precio}</th>
-              </tr>
-            )
-          }
-        </tbody>
-
-      </table>
-      <button onClick={eliminarUltimaFila}>Eliminar</button>
+      <Dado valor={valor[0]}/>
+      <Dado valor={valor[1]}/>
+      <Dado valor={valor[2]}/>
+      <button onClick={CambiarValor}>Cambio</button>
     </div>
   );
 }
